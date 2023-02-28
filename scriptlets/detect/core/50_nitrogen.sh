4f4646abd420bca2567516858e0978d8f28df830
Pierre Marzin <pierre.marzin@iot.bzh>
2023-02-28 14:58:27 +0100

    scriptlets: correct indent
    Signed-off-by: Pierre Marzin <pierre.marzin@iot.bzh>

--- scriptlets/detect/core/50_nitrogen.sh
+++ scriptlets/detect/core/50_nitrogen.sh
@@ -10,12 +10,12 @@ detect_nitrogen() {
 	[[ ! "$(cat /sys/firmware/devicetree/base/model)" =~ "Nitrogen" ]] && return 0;
 	info "Nitrogen family detected."
 
-    # soc information
-    keys[soc_vendor]="Nitrogen"
-    keys[soc_family]="$(readkey /sys/devices/soc0/family )"
-    keys[soc_id]="$(readkey /sys/devices/soc0/soc_id)"
-    keys[soc_name]="$(readkey /sys/devices/soc0/machine)"
-    keys[cpu_cache_kb]="unknown"
+	# soc information
+	keys[soc_vendor]="Nitrogen"
+	keys[soc_family]="$(readkey /sys/devices/soc0/family )"
+	keys[soc_id]="$(readkey /sys/devices/soc0/soc_id)"
+	keys[soc_name]="$(readkey /sys/devices/soc0/machine)"
+	keys[cpu_cache_kb]="unknown"
 	keys[gpu_name]=$(readkey /sys/firmware/devicetree/base/soc/gpu/compatible | cut -f 2- -d',')
 	keys[soc_revision]="$(readkey /sys/devices/soc0/revision)"
 
@@ -25,11 +25,11 @@ detect_nitrogen() {
 	then keys[cpu_freq_mhz]=$((${keys[cpu_freq_mhz]}/1000))
 	fi
 
-    local k1=$(grep OF_COMPATIBLE_0 /sys/devices/system/cpu/cpu0/uevent | cut -f2 -d',')
+	local k1=$(grep OF_COMPATIBLE_0 /sys/devices/system/cpu/cpu0/uevent | cut -f2 -d',')
 	keys[cpu_compatibility]="$k1"
 
-    # detect board
-    keys[board_model]=$(readkey /sys/firmware/devicetree/base/compatible | cut -f2 -d',')
+	# detect board
+	keys[board_model]=$(readkey /sys/firmware/devicetree/base/compatible | cut -f2 -d',')
 	# models=( $(tr '\0' '\n' </sys/firmware/devicetree/base/compatible | while IFS=',' read vendor model; do echo $model; done) )
 	# keys[board_model]=$(IFS=- ; echo "${models[*]}")
 	
